14b18ff6dcda98266a17451fa36b4448a6b70600
Sergey Zaharco <zaharco17@gmal.com>
2021-05-21 06:18:02 +0300

    добавил строку

--- types.py
+++ types.py
@@ -1,8 +1,9 @@
 a = 2
 b = 0.5
+
 print(a + b)
-print( float('1'))  # ???
-print( int('2'))  # ???
-print( bool(1))  # ???
-print( bool(''))  # ???
+print( float('1'))  
+print( int('2'))  
+print( bool(1))  
+print( bool(''))  
 print( bool(0))  
